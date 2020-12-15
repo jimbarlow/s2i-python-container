0d9d50f07992494d8d4f3eb285393b3b96c00eeb
jimbarlow <jim@jimbarlow.com>
2020-12-15 23:16:47 +0000

    added file listing in the data volume

--- examples/setup-test-app/testapp.py
+++ examples/setup-test-app/testapp.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -7,14 +8,21 @@ api = Api(application)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello World from a generic Universal Base Image optimized for RHEL! try hitting this resource at /python_version'
+        return 'Hello Another World from a generic Universal Base Image optimized for RHEL! try hitting this resource at /python_version'
 
 class PythonVersion (Resource):
     def get(self):
         return (sys.version)
 
+class ListFiles (Resource):
+    def get(self):
+        file_list = os.listdir( '/datafiles' )
+        listToStr = ' '.join(map(str, file_list))
+        return listToStr
+
 api.add_resource(Greeting, '/')
 api.add_resource(PythonVersion, '/python_version')
+api.add_resource(ListFiles, '/data_files')
 
 if __name__ == '__main__':
     application.run('0.0.0.0','8080')
