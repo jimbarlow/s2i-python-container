91b9ebd0f75434865eaf6cbfdf7c92dfb5166d6f
jimbarlow <jim@jimbarlow.com>
2020-12-15 22:47:41 +0000

    replaced sample app

--- examples/setup-test-app/testapp.py
+++ examples/setup-test-app/testapp.py
@@ -1,4 +1,20 @@
+import sys, os
+from flask import Flask, request
+from flask_restful import Resource, Api
 
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type','text/plain')])
-    return [b"Hello from gunicorn WSGI application!"]
+application = Flask(__name__)
+api = Api(application)
+
+class Greeting (Resource):
+    def get(self):
+        return 'Hello World from a generic Universal Base Image optimized for RHEL! try hitting this resource at /python_version'
+
+class PythonVersion (Resource):
+    def get(self):
+        return (sys.version)
+
+api.add_resource(Greeting, '/')
+api.add_resource(PythonVersion, '/python_version')
+
+if __name__ == '__main__':
+    application.run('0.0.0.0','8080')
